408fadb42b7aa142c4b6f84f2c42ea7c8f558181
teruya-sawamura <t.s.16125@gmail.com>
2020-06-05 23:54:10 +0900

    api test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'goods#search'
+  get 'goods/search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
